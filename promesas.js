72067362c0bb9fe0aed39d0a397aa9615b9cdfd3
JuanGaybre <jugaybre@gmail.com>
2020-01-14 18:51:40 -0600

    Añadiendo ejemplos y uso de Promise.all y Promise.race

--- promesas.js
+++ promesas.js
@@ -1,12 +1,12 @@
 const getStatus = new Promise((resolve, reject) => {
+	let indicator = Math.random()
 	setTimeout(() => {
-		var indicator = Math.random()
 		if (indicator > .5) {
+			console.log(indicator)
 			resolve('Todo va Bien')
-			console.log(indicator)
 		}else {
-			reject('Todo va Mal')
 			console.log(indicator)
+			reject('Todo va Mal')
 		}
 	}, 1000)
 })
@@ -19,10 +19,10 @@ getStatus
 	})
 
 
+
 const diasSemana = new Promise((ok, fail) => {
 	const semanaCompleta = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
-	var random = Math.random()
-	
+	let random = Math.random()	
 	setTimeout(() => {
 		if (random > .5) {
 			console.log(random)
@@ -38,9 +38,64 @@ diasSemana
 		console.log('Los dias de la semana son:')
 		console.log(data)
 	})
-	.catch((data) => {
-		console.log(data)
-	})
+	.catch((data) => console.log(data))
 
 
-//const carrera =
+const proyecto = new Promise((completado, incompleto) => {
+	let random = Math.random()
+	setTimeout(() => {
+		if (random > .5) {
+			console.log(random)
+			completado('El proyecto se entrego completo y a Tiempo')
+		}else{
+			console.log(random)
+			incompleto('Eres un bastardo y no cumpliste tu deber')
+		}
+	}, 3000)
+})
+proyecto
+	.then((message) => console.log(message))
+	.catch((message) => console.log(message))
+
+
+const vacaciones = new Promise((aprobado, denegado) => {
+	let random = Math.random()
+	setTimeout(() => {
+		if (random > .5) {
+			console.log(random)
+			aprobado('Anda a descansar que te lo has ganado')
+		}else{
+			console.log(random)
+			denegado('No podrás salir de vacaciones en las fechas solicitadas')
+		}
+	}, 4000)
+})
+vacaciones
+	.then((msj) => console.log(msj))
+	.catch((msj) => console.log(msj))
+
+
+/*
+Promise.all ejecutará todas las promesas y en cuantio termine mostrará el resultado
+Promise.all([
+	getStatus, 
+	diasSemana, 
+	proyecto, 
+	vacaciones
+])
+.then((message) => console.log(message))
+.catch((message) => console.log(message))
+*/
+
+
+/*
+Promise.race ejecutará solamente la Promesa que primero se resuelva
+Promise.race([
+	getStatus,
+	diasSemana,
+	proyecto,
+	vacaciones
+	])
+.then((message) => console.log(message))
+.catch((message) => console.log(message))
+*/
